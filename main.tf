d246530cb94e35a128fff844ca401274ee376ea4
Johannes Scheuerer <joscheuerer@gmail.com>
2023-10-16 16:40:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -588,6 +588,6 @@ resource "aws_iam_instance_profile" "this" {
 ################################################################################
 
 resource "aws_ec2_instance_state" "state" {
-  instance_id = aws_instance.this[*].id
+  instance_id = "${aws_instance.this.*.id[count.index]}"
   state       = var.instance_state
 }
