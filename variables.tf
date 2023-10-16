b957607b5b7a51211a02e726757605ad85154e3b
Johannes Scheuerer <joscheuerer@gmail.com>
2023-10-16 16:02:53 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@ variable "instance_state" {
   description = "State of instance"
   type = string
   default = "running" # or "stopped"
+}
 
 variable "name" {
   description = "Name to be used on EC2 instance created"
